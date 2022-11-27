9c091352607ce8aee5a1f268c11d2ff6b0c32d62
Benjamin Dos Santos <hello@benjamindossantos.be>
2022-11-27 14:50:50 +0100

    refactor: remove repository pattern implementation

--- app/Controllers/Http/CustomersController.ts
+++ app/Controllers/Http/CustomersController.ts
@@ -1,16 +1,12 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import CreateCustomerValidator from '../../Validators/CreateCustomerValidator'
 import UpdateCustomerValidator from '../../Validators/UpdateCustomerValidator'
-import CustomerRepository from '../../Repositories/CustomerRepository'
-import { inject } from '@adonisjs/core/build/standalone'
 import { CustomerRoutes, CustomerViews } from '../../Enums/CustomerEnums'
+import Customer from '../../Models/Customer'
 
-@inject()
 export default class CustomersController {
-  constructor(private customerRepository: CustomerRepository) {}
-
   public async index({ view, auth }: HttpContextContract): Promise<string> {
-    const customers = await this.customerRepository.findAllBy({ userId: auth.user.id })
+    const customers = await Customer.query().where('user_id', auth.user.id)
 
     return view.render(CustomerViews.INDEX, { customers })
   }
@@ -21,41 +17,41 @@ export default class CustomersController {
 
   public async store({ response, request, auth }: HttpContextContract): Promise<void> {
     const payload = await request.validate(CreateCustomerValidator)
-    const customer = await this.customerRepository.create({ ...payload, userId: auth.user.id })
+    const customer = await Customer.create({ ...payload, userId: auth.user.id })
 
-    response.redirect().toRoute(CustomerRoutes.SHOW, [customer.id])
+    return response.redirect().toRoute(CustomerRoutes.SHOW, [customer.id])
   }
 
   public async show({ view, params, bouncer }: HttpContextContract): Promise<string> {
-    const customer = await this.customerRepository.find(params.id)
+    const customer = await Customer.findOrFail(params.id)
     await bouncer.with('CustomerPolicy').authorize('view', customer)
 
     return view.render(CustomerViews.SHOW, { customer })
   }
 
   public async edit({ view, params, bouncer }: HttpContextContract): Promise<string> {
-    const customer = await this.customerRepository.find(params.id)
+    const customer = await Customer.findOrFail(params.id)
     await bouncer.with('CustomerPolicy').authorize('update', customer)
 
     return view.render(CustomerViews.EDIT, { customer })
   }
 
   public async update({ request, response, params, bouncer }: HttpContextContract): Promise<void> {
-    const customer = await this.customerRepository.find(params.id)
+    const customer = await Customer.findOrFail(params.id)
     await bouncer.with('CustomerPolicy').authorize('update', customer)
 
     const payload = await request.validate(UpdateCustomerValidator)
     await customer.merge(payload).save()
 
-    response.redirect().toRoute(CustomerRoutes.SHOW, [params.id])
+    return response.redirect().toRoute(CustomerRoutes.SHOW, [params.id])
   }
 
   public async destroy({ response, params, bouncer }: HttpContextContract): Promise<void> {
-    const customer = await this.customerRepository.find(params.id)
+    const customer = await Customer.findOrFail(params.id)
     await bouncer.with('CustomerPolicy').authorize('delete', customer)
 
     await customer.delete()
 
-    response.redirect().toRoute(CustomerRoutes.INDEX)
+    return response.redirect().toRoute(CustomerRoutes.INDEX)
   }
 }
--- app/Repositories/CustomerRepository.ts
+++
@@ -1,6 +0,0 @@
-import Customer from '../Models/Customer'
-import BaseRepository from './BaseRepository'
-
-export default class CustomerRepository extends BaseRepository<typeof Customer> {
-  protected readonly model = Customer
-}
